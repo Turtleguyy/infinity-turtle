4d6b85998e2823bf50f74434ff2ebbd75861c330
Zachary Nicoll <zacharynicoll@gmail.com>
2014-09-01 17:37:13 -0400

    More progress towards a demo

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,11 +3,24 @@ $(function() {
   $.getJSON(url + '?limit=100&offset=0&callback=?', function(data) {
     var turtle = new InfinityTurtle(data.results);
     turtle.promise.progress(function(data) {
-      console.log('new page', data);
+      renderData(data);
     });
 
     turtle.promise.done(function(data) {
-      console.log('last page', data);
+      renderData(data, true);
     });
   });
+
+  var renderData = function(data, done) {
+    for (var i = 0; i < data.length; i++) {
+      var title = '<h2>' + data[i].title + '</h2>';
+      var body  = '<p>' + data[i].body + '</p>';
+      var html  = '<div class="petition">' + title + body + '</div>';
+      $('section').append(html);
+    }
+
+    if (done) {
+      $('section').append('<p>All done.</p>');
+    }
+  };
 });
